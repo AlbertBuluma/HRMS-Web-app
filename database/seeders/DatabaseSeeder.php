464d31125fcd1be77329fe44f9c7c1752888f1de
AlbertBuluma <albert.buluma1@gmail.com>
2024-10-07 16:39:13 +0300

    Modify database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Staff;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'administrator',
+            'email' => 'administrator@email.com',
+            'password' => Hash::make('password'),
+            'remember_token' => 'ZCOibwOCHn',
         ]);
+
+        Staff::factory(20)->create();
     }
 }
