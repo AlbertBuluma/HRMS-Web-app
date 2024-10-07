04cc8195654fb39b70e6f175718380385ed7feaa
AlbertBuluma <albert.buluma1@gmail.com>
2024-10-07 23:49:14 +0300

    Add Staff CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [StaffController::class, 'index'])->name('dashboard');
+    Route::resource('/staff', StaffController::class);
+});
+
 
 require __DIR__.'/auth.php';
